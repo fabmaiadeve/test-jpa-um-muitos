3e7b8ebbf9d1536733cb5ba4bca67c678fdb25f6
fabmaiadev <fabxmaia@outlook.com>
2023-03-19 21:17:30 -0300

    feature main/fabiano: 6° commit: implementando nos controladores de cliente e endereco as funcionalidades de alterar e deletar

--- src/main/java/com/fabdev/testjpaummuitos/controllers/AdressController.java
+++ src/main/java/com/fabdev/testjpaummuitos/controllers/AdressController.java
@@ -8,9 +8,11 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +40,7 @@ public class AdressController {
 		
 		Customer customer = findCustomerBeforeInsert(adressDTO.getCustomerId());
 		
-		Adress adress = new Adress(adressDTO.getPublic_place(), adressDTO.getNeighborhood(), adressDTO.getCity(), adressDTO.getTypeAdress(), customer);
-		
+		Adress adress = new Adress(adressDTO.getPublic_place(), adressDTO.getNeighborhood(), adressDTO.getCity(), adressDTO.getTypeAdress(), customer);		
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(adressService.save(adress));
 	}
@@ -61,6 +62,37 @@ public class AdressController {
 		return ResponseEntity.status(HttpStatus.OK).body(optionalAdress.get());
 	}
 	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteAdress(@PathVariable(value = "id") UUID id) {
+		
+		Optional<Adress> optionalAdress = adressService.findById(id);
+		
+		if(optionalAdress.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Adress not found");
+		}
+		
+		adressService.delete(optionalAdress.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Adress record deleted successfully!");
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> updateAdress(@PathVariable(value = "id") UUID id,
+											   @RequestBody @Valid AdressDTO adressDTO) {
+		
+		Customer customer = findCustomerBeforeInsert(adressDTO.getCustomerId());
+		
+		Optional<Adress> optionalAdress = adressService.findById(id);
+		
+		if(optionalAdress.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Adress not found");
+		}
+		
+		Adress adress = new Adress(adressDTO.getPublic_place(), adressDTO.getNeighborhood(), adressDTO.getCity(), adressDTO.getTypeAdress(), customer);
+		adress.setId(optionalAdress.get().getId());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(adressService.save(adress));
+	} 
 	
 	
 	public Customer findCustomerBeforeInsert(UUID customer_id) throws IllegalArgumentException{
--- src/main/java/com/fabdev/testjpaummuitos/controllers/CustomerController.java
+++ src/main/java/com/fabdev/testjpaummuitos/controllers/CustomerController.java
@@ -8,9 +8,11 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +58,37 @@ public class CustomerController {
 		return ResponseEntity.status(HttpStatus.OK).body(customerOptional.get());
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteCustomer(@PathVariable(value = "id") UUID id) {
+		
+		Optional<Customer> customerOptional = customerService.findById(id);
+		
+		if(customerOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+		}
+		
+		if(!customerOptional.get().getAdresses().isEmpty()) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("It's not possible to delete the record because the key exists in another table.");
+		}
+		
+		customerService.delete(customerOptional.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Customer record deleted successfully!");
+	}
 	
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> update(@PathVariable(value = "id") UUID id,
+										 @RequestBody @Valid CustomerDTO customerDTO) {
+		
+		Optional<Customer> customerOptional = customerService.findById(id);
+		
+		if(customerOptional.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+		}
+		
+		Customer customer = new Customer(customerDTO.getName(), customerDTO.getEmail());
+		customer.setId(customerOptional.get().getId());
+		
+		return ResponseEntity.status(HttpStatus.OK).body(customerService.save(customer));
+	}
 
 }
